6f54a30d59f04b29ca7a49f9f65f85925c681521
alain M samoun <61603038+alaintanguy@users.noreply.github.com>
2026-04-15 16:16:27 -0700

    Refactor package and import statements in MainActivity
    Added package declaration and updated import statement.

--- app/src/main/java/com/healthbridge/MainActivity.kt
+++ app/src/main/java/com/healthbridge/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.healthbridge.R
+package com.healthbridge
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.healthbridge.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Link to your layout file
         setContentView(R.layout.activity_main)
     }
 }
