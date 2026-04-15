c9f419a16014e378869eb4dffd527b383c465ab8
alain M samoun <samoun.alain@gmail.com>
2026-04-14 18:06:38 -0700

    Add missing dependencies (appcompat + okhttp)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 }
 
 dependencies {
+	 implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
